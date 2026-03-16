5487c9082efc4d6be4a36d96ef199629c7e06b7b
0xkephra <0xkephra@proton.me>
2026-03-16 12:37:09 +0000

    Delete practice_3.py

--- practice_3.py
+++
@@ -1,9 +0,0 @@
-# EXERCISE 1 AT https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
-
-from datetime import datetime
-name = input('May i know your name please ? : ')
-age = int(input('May i know your age please ? : '))
-age_till_hundred = 100 - age
-current_year = datetime.now().year
-print(
-    f'{name} will turn 100 years old the year {age_till_hundred + current_year}')
